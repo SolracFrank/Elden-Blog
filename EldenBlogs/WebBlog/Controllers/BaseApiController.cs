ce3775fc00e7c4edcee3a2aed651c1c2a5d880b3
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-25 17:26:24 -0600

    Add: Testing Base Controller

---
+++ EldenBlogs/WebBlog/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebBlog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> BaseEndpoint()
+        {
+            return Ok("HI, I'm an Endpoint");
+        }
+    }
+}
