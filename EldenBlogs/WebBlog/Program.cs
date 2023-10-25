98f3576007a4aa3df59eeaf866021815832dd41e
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-25 17:26:08 -0600

    Configure: Program for Swagger and Controllers use
    Program has been configured for using Swagger and Controllers.

--- EldenBlogs/WebBlog/Program.cs
+++ EldenBlogs/WebBlog/Program.cs
@@ -1,6 +1,33 @@
+
+
 var builder = WebApplication.CreateBuilder(args);
+var isDevelopment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == Environments.Development;
+
+//build configurations
+
+#region Controllers
+builder.Services.AddControllers();
+#endregion
+
+#region Swagger builder
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+#endregion
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app configurations
+
+if (app.Environment.IsStaging() || app.Environment.IsDevelopment())
+{
+    #region Swagger App
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    #endregion
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
