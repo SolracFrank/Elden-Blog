d0b6dee356fb5fa0c2af222f551a8e53ade1dc21
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-26 13:07:51 -0600

    Feat: Configure mock controller for Api Versioning

---
+++ EldenBlogs/WebBlog/Controllers/V1/BaseApiController.cs
@@ -0,0 +1,25 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace WebBlog.Controllers.V1
+{
+    [ApiVersion(1.0)]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+        [HttpGet]
+        [SwaggerResponse(StatusCodes.Status200OK, "Test", typeof(string))]
+        public async Task<IActionResult> BaseEndpoint()
+        {
+            return Ok("HI, I'm an Endpoint");
+        }
+        [HttpGet("otro")]
+        [ProducesResponseType(typeof(string), 200)]
+        public async Task<IActionResult> BaseEndpoint2()
+        {
+            return Ok("HI, I'm an Endpoint 2");
+        }
+    }
+}
