3c541dcd5ba0df46724928278b06ef3cb6bf30f7
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 12:47:48 -0600

    Add: Blog Settings Configuration

---
+++ EldenBlogs/Infrastructure/Data/Configurations/BlogSettingsConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Data.Configurations
+{
+    public class BlogSettingsConfiguration : IEntityTypeConfiguration<BlogSetting>
+    {
+        public void Configure(EntityTypeBuilder<BlogSetting> builder)
+        {
+            builder.ToTable(nameof(BlogSetting));
+
+            builder.HasKey(b => b.Id);
+
+            builder.Property(b => b.Id)
+                .IsRequired()
+                .HasDefaultValueSql("newid()");
+
+            builder.Property(b => b.IsMature)
+                .IsRequired()
+                .HasDefaultValue(false);
+
+            builder.HasOne<Blog>()
+                .WithOne()
+                .HasForeignKey<BlogSetting>(b => b.Fk_Blog_Id)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("fk_blog_blogsettings");
+
+        }
+    }
+}
