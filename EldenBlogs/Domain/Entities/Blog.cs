a0baf6597ee39311bb9c47fc749597e4fe23354a
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 11:30:20 -0600

    Feat: Refactor Blog Entity

--- EldenBlogs/Domain/Entities/Blog.cs
+++ EldenBlogs/Domain/Entities/Blog.cs
@@ -3,7 +3,12 @@
     public class Blog
     {
         public int Id { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogDescription { get; set; }
+        public string? BannerPath { get; set; }
+        public required string BlogTitle { get; set; }
+        public string? BlogDescription { get; set; }
+        public required DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+        public Guid Fk_Id_User { get; set; }
+
     }
 }
