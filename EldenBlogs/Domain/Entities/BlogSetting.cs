c6cdeb65f67fd6a6f1a1a01b9ef2d0b401292448
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 11:31:25 -0600

    Add: BlogSettings Entity

---
+++ EldenBlogs/Domain/Entities/BlogSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class BlogSetting
+    {
+        public Guid Id { get; set; }
+        public bool IsMature { get; set; }
+        public int Fk_Blog_Id { get; set; }
+    }
+}
