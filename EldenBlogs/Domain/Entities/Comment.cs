f1763fe7ada32c4b87da7911b0fc040293ae202e
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 11:31:10 -0600

    Add: Comment Entity

---
+++ EldenBlogs/Domain/Entities/Comment.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public required string CommentContent { get; set; }
+        public int Likes { get; set; } = 0;
+        public int Dislikes { get; set; } = 0;
+        public required Guid Fk_Id_User { get; set; }
+    }
+}
