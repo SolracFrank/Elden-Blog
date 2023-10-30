58fbf8eb37bfe22245351d160008f60332b83d00
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 13:02:58 -0600

    Add: Infrastructure Dependency Injection

--- EldenBlogs/Infrastructure/DependencyInjections.cs
+++ EldenBlogs/Infrastructure/DependencyInjections.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.Data;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace Infrastructure
 {
@@ -7,6 +10,14 @@ namespace Infrastructure
     {
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                builder =>
+                {
+                    builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName);
+                    builder.EnableRetryOnFailure(4, TimeSpan.FromSeconds(5), null);
+                }));
+
 
         }
     }
