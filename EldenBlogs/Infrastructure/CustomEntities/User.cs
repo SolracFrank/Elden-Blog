de6be52ba953797d4b950a5a1fac6fbfa669dbb9
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 11:30:41 -0600

    Add: User Entity

---
+++ EldenBlogs/Infrastructure/CustomEntities/User.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Infrastructure.CustomEntities
+{
+    public class User : IdentityUser
+    {
+        private int _age;
+        public required DateTime BirthDate { get; set; }
+        public int Age
+        {
+            get
+            {
+                _age = DateTime.Now.Year - BirthDate.Year;
+                if (DateTime.Now.DayOfYear < BirthDate.DayOfYear)
+                    _age = _age - 1;
+
+                return _age;
+            }
+        }
+        public string? Avatar { get; set; }
+
+    }
+}
