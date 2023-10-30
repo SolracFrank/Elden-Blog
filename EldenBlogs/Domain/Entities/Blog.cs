cc15214d685e6dcf739b14932e20d309bf044405
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 10:32:55 -0600

    Add: Blog Entity

---
+++ EldenBlogs/Domain/Entities/Blog.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Entities
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogDescription { get; set; }
+    }
+}
