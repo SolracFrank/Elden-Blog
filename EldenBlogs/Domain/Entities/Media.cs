18ab166f9b4da22c3b7bf6eaf101d598adbd38b3
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-10-30 11:30:49 -0600

    Add: Media Entity

---
+++ EldenBlogs/Domain/Entities/Media.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Media
+    {
+        public int Id_Media { get; set; }
+        public enum Type { Video,Image}
+        public required string MediaType { get; set; }
+        public required string MediaPath { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
