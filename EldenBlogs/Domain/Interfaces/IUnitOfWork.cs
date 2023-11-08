eaa871c4a998ce1eb23fbe26ceb1f1b5454dfb7b
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-11-08 09:06:51 -0600

    Add: Repository pattern interfaces

---
+++ EldenBlogs/Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Blog> Blogs { get; }
+        IRepository<BlogSetting> BlogSettings { get; }
+        IRepository<Comment> Comments { get; }
+        IRepository<Entry> Entries { get; }
+        IRepository<Media> Medias { get; }
+        bool SaveChanges();
+        Task<bool> SaveChangesAsync(CancellationToken cancellationToken);
+        public Task BeginTransactionAsync();
+        public Task CommitTransactionAsync(CancellationToken cancellationToken);
+        public void RollbackTransaction();
+        public Task<T> ExecuteWithinTransactionAsync<T>(Func<Task<T>> action);
+
+    }
+}
