5b805ed233330be489a00d4a44cd1727dff134ac
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-11-08 14:49:04 -0600

    Add: EntityValidator util

---
+++ EldenBlogs/Infrastructure/Utils/EntityValidator.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure.Utils
+{
+    public static class EntityValidator
+    {
+        public static void EnsureDoesNotExist<T>(this T entity, ILogger logger) where T : class
+        {
+            if (entity != null)
+            {
+                var entityName = typeof(T).Name;
+                logger.LogInformation($"{entityName} already exists");
+                throw new BadRequestException($"{entityName} already exists");
+            }
+        }
+
+    }
+}
