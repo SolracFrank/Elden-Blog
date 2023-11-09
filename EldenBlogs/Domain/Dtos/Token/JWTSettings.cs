6c40d62f625dd0db0e2fbef0a7fb75399140ae3e
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-11-09 10:23:12 -0600

    Add: JWTSettings entity

---
+++ EldenBlogs/Domain/Dtos/Token/JWTSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Dtos.Token
+{
+    public class JWTSettings
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public int DurationInMinutes { get; set; }
+
+    }
+}
