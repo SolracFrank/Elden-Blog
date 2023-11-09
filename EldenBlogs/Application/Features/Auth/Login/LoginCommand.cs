29166264385e6238f5eb236a15b23c213361819e
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-11-09 10:26:56 -0600

    Add: Login behavior

---
+++ EldenBlogs/Application/Features/Auth/Login/LoginCommand.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Dtos.Token;
+using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Features.Auth.Login
+{
+    public class LoginCommand : IRequest<Result<JWTResult>>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Ip { get; set; }
+    }
+}
---
+++ EldenBlogs/Application/Features/Auth/Login/LoginCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+
+namespace Application.Features.Auth.Login
+{
+    public class LoginCommandValidator : AbstractValidator<LoginCommand>
+    {
+        public LoginCommandValidator()
+        {
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(r => r.Email)
+                .NotEmpty().WithMessage("{PropertyName} is required")
+                .EmailAddress().WithMessage("{PropertyName} is invalid");
+
+
+            RuleFor(r => r.Password)
+                .NotEmpty().WithMessage("{PropertyName} is required");
+        }
+    }
+
+    
+    
+}
