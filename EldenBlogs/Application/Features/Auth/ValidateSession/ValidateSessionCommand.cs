69cc0dc348333cf1776c3a64d003428987c79461
SolracFrank <carlosfranciscoherrerafranco@gmail.com>
2023-12-23 13:00:45 -0600

    add: session validation CQRS

---
+++ EldenBlogs/Application/Features/Auth/ValidateSession/ValidateSessionCommand.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Dtos.Auth;
+using LanguageExt.Common;
+using MediatR;
+
+namespace Application.Features.Auth.ValidateSession
+{
+    public class ValidateSessionCommand : IRequest<Result<SessionValidationResult>>
+    {
+        public string UserId { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
---
+++ EldenBlogs/Application/Features/Auth/ValidateSession/ValidateSessionCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Application.Features.Auth.Login;
+using Application.Interfaces.AuthServices;
+using Domain.Dtos.Auth;
+using LanguageExt.Common;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Application.Features.Auth.ValidateSession
+{
+    public class ValidateSessionCommandHandler : IRequestHandler<ValidateSessionCommand, Result<SessionValidationResult>>
+    {
+        private readonly ILogger<LoginCommandHandler> _logger;
+        private readonly IValidateSessionService _validateSession;
+
+        public ValidateSessionCommandHandler(ILogger<LoginCommandHandler> logger, IValidateSessionService validateSession)
+        {
+            _logger = logger;
+            _validateSession = validateSession;
+        }
+
+        public async Task<Result<SessionValidationResult>> Handle(ValidateSessionCommand request, CancellationToken cancellationToken)
+        {
+            var isSessionValidated = await _validateSession.ValidateSession(new ValidateSessionCommand { UserId = request.UserId, RefreshToken = request.RefreshToken }, cancellationToken);
+
+            var sessionValidationResult = new SessionValidationResult
+            {
+                IsValidated = isSessionValidated
+            };
+
+            return new Result<SessionValidationResult>(sessionValidationResult);
+        }
+    }
+}
